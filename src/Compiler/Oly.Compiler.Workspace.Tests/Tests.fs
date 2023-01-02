f28909875307be579530288d47b6c2eedd3272af
TIHan <lol.tihan@gmail.com>
2023-01-02 14:53:38 -0800

    Upgrading to net7

--- src/Compiler/Oly.Compiler.Workspace.Tests/Tests.fs
+++ src/Compiler/Oly.Compiler.Workspace.Tests/Tests.fs
@@ -32,7 +32,7 @@ let createDocument src (workspace: OlyWorkspace) =
     let isDebuggable = false
 #endif
     let projOptions = OlyProjectConfiguration("olytest", ImArray.empty, isDebuggable)
-    let sol, proj = workspace.Solution.CreateProject(OlyPath.Create "olytest", projOptions, "dotnet", OlyTargetInfo("net6", OlyOutputKind.Executable), CancellationToken.None)
+    let sol, proj = workspace.Solution.CreateProject(OlyPath.Create "olytest", projOptions, "dotnet", OlyTargetInfo("net7", OlyOutputKind.Executable), CancellationToken.None)
     let syntaxTree = OlySyntaxTree.Parse(OlyPath.Create "olytest", (fun _ -> OlySourceText.Create(src)))
     let sol, proj, doc = sol.UpdateDocument(proj.Path, OlyPath.Create "olytest", syntaxTree, ImArray.empty)
     doc
