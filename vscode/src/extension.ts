ce5856b84a2c4b2f9ec14d6680e070f53ab1f3d2
TIHan <lol.tihan@gmail.com>
2023-01-02 15:38:21 -0800

    Minor cleanup

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -432,7 +432,7 @@ module OlyClientCommands {
 
 export function activate(context: ExtensionContext) {
 	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'net7', 'Oly.LanguageServer.dll')
+		path.join('out', 'net7', 'Oly.LanguageServer.dll')
 	);
 
 	// If the extension is launched in debug mode then the debug server options are used
