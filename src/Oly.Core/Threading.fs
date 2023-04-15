ffd3627a94f888d46bc5edcf22db0d439149b84c
TIHan <lol.tihan@gmail.com>
2023-04-15 10:48:51 -0700

    Forgot to add Threading.fs

---
+++ src/Oly.Core/Threading.fs
@@ -0,0 +1,27 @@
+﻿namespace Oly.Core
+
+open System
+open System.Threading.Tasks
+
+#if DEBUG
+/// Only used in DEBUG builds to prevent stack-overflows when tail calls are disabled.
+[<AbstractClass;Sealed>]
+type DebugStackGuard =
+
+    static member MaxDepth with get() = 3
+
+    [<DefaultValue(false)>]
+    [<ThreadStatic>]
+    static val mutable private CurrentDepth: int32
+
+    static member Do<'T>(f: unit -> 'T) =
+        DebugStackGuard.CurrentDepth <- DebugStackGuard.CurrentDepth + 1
+        try
+            if DebugStackGuard.CurrentDepth % DebugStackGuard.MaxDepth = 0 then
+                Task.Run(fun () -> f()).Result
+            else
+                f()
+        finally
+            DebugStackGuard.CurrentDepth <- DebugStackGuard.CurrentDepth - 1
+#endif
+
