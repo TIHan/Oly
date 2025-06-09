c37d15ffbf795895e973c46ddd55528feddfefe1
TIHan <lol.tihan@gmail.com>
2025-06-08 22:47:04 -0700

    Trying to build the Oly CLI in Oly

--- src/olylib/Library.fs
+++ src/olylib/Library.fs
@@ -1,27 +1,28 @@
 ﻿namespace olylib
 
-open System
 open System.IO
 open System.Threading
-open System.Threading.Tasks
-open System.Runtime.CompilerServices
-open System.Text.Json.Serialization
 open Oly.Core
 open Oly.Compiler.Workspace
 open Oly.Runtime.Target.DotNet
 open Oly.Runtime.Target.Interpreter
+open Oly.Runtime.Target.Spirv
+open Oly.Compiler.Workspace.Service
 
 module Oly =
 
     let Build (configName: string, projectPath: OlyPath, ct: CancellationToken) =
-        // TODO: Fix this.
-        let rs = OlyWorkspaceResourceSnapshot.Create(OlyPath.Empty)
+        let projectPath = OlyPath.Combine(OlyPath.Create(System.Environment.CurrentDirectory), projectPath)
+        let rootPath = OlyPath.GetDirectory(projectPath)
+        let activeConfigPath = OlyPath.Combine(rootPath, ".olyworkspace/state.json")
+        File.WriteAllText(activeConfigPath.ToString(), $"""{{ "activeConfiguration": "{configName}" }}""")
 
         let targets = 
             [
                 InterpreterTarget() :> OlyBuild
                 DotNetTarget()
+                SpirvTarget()
             ] |> ImArray.ofSeq
         let workspace = OlyWorkspace.Create(targets)
-        workspace.UpdateDocument(rs, projectPath, rs.GetSourceText(projectPath), ct)
-        workspace.BuildProjectAsync(rs, projectPath, ct)
+        let listener = OlyWorkspaceListener(workspace, System.Lazy<_>.CreateFromValue(rootPath))
+        workspace.BuildProjectAsync(listener.ResourceSnapshot, projectPath, ct)
