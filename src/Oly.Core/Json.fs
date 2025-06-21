3b59d90a42434891325fcc24438d79c32baecea1
TIHan <lol.tihan@gmail.com>
2025-06-20 19:57:26 -0700

    Forgot to add Json.fs

---
+++ src/Oly.Core/Json.fs
@@ -0,0 +1,17 @@
+﻿module Oly.Core.Json
+
+open System
+open System.IO
+open System.Text.Json
+open System.Text.Json.Serialization
+open System.Text.Json.Serialization.Metadata
+
+let Deserialize<'T>(utf8JsonStream: Stream) =
+    let jsonOptions = JsonSerializerOptions()
+    jsonOptions.PropertyNameCaseInsensitive <- true
+    //jsonOptions.TypeInfoResolver <-
+    //    { new Metadata.IJsonTypeInfoResolver with
+    //            member this.GetTypeInfo(``type``: Type, options: JsonSerializerOptions): Metadata.JsonTypeInfo = 
+    //                raise (System.NotImplementedException(``type``.ToString())) }
+    JsonSerializer.Deserialize<'T>(utf8JsonStream, jsonOptions)
+
